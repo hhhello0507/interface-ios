a6fa5c19978071d0b4e5f1717a08983e6b3aaa69
hhhello <hhhello0507@dgsw.hs.kr>
2025-03-25 20:06:40 +0900

    Fix advancedFocus access modifier

--- Sources/Interface/Component/TextField/Internal/View+advancedFocus.swift
+++ Sources/Interface/Component/TextField/Internal/View+advancedFocus.swift
@@ -7,8 +7,7 @@
 
 import SwiftUI
 
-struct AdvancedFocusViewModifier: ViewModifier {
-    
+internal struct AdvancedFocusViewModifier: ViewModifier {
     @FocusState private var focused: Bool
     
     func body(content: Content) -> some View {
@@ -20,7 +19,7 @@ struct AdvancedFocusViewModifier: ViewModifier {
     }
 }
 
-public extension View {
+internal extension View {
     func advancedFocus() -> some View {
         self.modifier(AdvancedFocusViewModifier())
     }
